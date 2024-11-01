3d6aa0ecafd0e2a02f26f059a3d66e6eaf9fc2c7
Pablo Vinas <pablo.araujo@alu.ufc.br>
2024-11-01 14:14:34 -0300

    commit IO

---
+++ src/IO.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class IO {
+
+    private static Scanner scan = new Scanner(System.in);
+
+    public static String input(){
+        return scan.nextLine();
+    }
+}
